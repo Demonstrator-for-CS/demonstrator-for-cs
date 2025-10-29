568a48f0d17a9fda6a6e79fee07898a78ee5b47d
Luke135 <lhaidze@gmail.com>
2025-10-29 15:18:29 -0400

    Base for the controller for future modification. This is not the final product. Used React with VIT framework. As you will see, I did not change much

---
+++ demo_controller/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import pittLogo from './images/pitt_logo.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [background, setBackground] = useState('#ffffff');
+
+  return (
+    <>
+      <div>
+        <a href="https://www.pitt.edu/" target="_blank">
+          <img src={pittLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Demonstrator Controller (Name subject to change obv)</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)} style = {{color: '#61dafbaa'}}>
+          6
+        </button>
+        <button onClick={() => setCount((count) => count - 1)} style = {{color: '#61dafbaa'}}>
+          7
+        </button>
+        <p>
+          Code is in <code>src/App.jsx</code> 
+        </p>
+      </div>
+      <p className="read-the-docs">
+        This will eventually be the controller for the CS Demonstrator. Will add more buttons for users to control the demos.
+      </p>
+    </>
+  )
+}
+
+export default App
