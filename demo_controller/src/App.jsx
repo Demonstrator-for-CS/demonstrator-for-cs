34ed940ada929e781d5c12b8b13b9f435116fdf4
Luke135 <lhaidze@gmail.com>
2025-11-07 15:56:15 -0500

    controllers pretty much done, may need to come back for adder

--- demo_controller/src/App.jsx
+++ demo_controller/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import pittLogo from './images/pitt_logo.png'
 import './App.css'
 
 function App() {
@@ -10,24 +8,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://www.pitt.edu/" target="_blank">
-          <img src={pittLogo} className="logo" alt="Vite logo" />
-        </a>
       </div>
-      <h1>Demonstrator Controller (Name subject to change obv)</h1>
+      <h3>Select Demo</h3>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} style = {{color: '#61dafbaa'}}>
-          6
-        </button>
-        <button onClick={() => setCount((count) => count - 1)} style = {{color: '#61dafbaa'}}>
-          7
-        </button>
-        <p>
-          Code is in <code>src/App.jsx</code> 
-        </p>
+        <button className = "navigate-button"> ← </button>
+        <button className = "navigate-button"> Select </button>
+        <button className = "navigate-button"> → </button>
       </div>
-      <p className="read-the-docs">
-        This will eventually be the controller for the CS Demonstrator. Will add more buttons for users to control the demos.
+      <p className="read-the-docs" id = 'instructions'>
+        Use these buttons to navigate through the demonstrator!
       </p>
     </>
   )
--- demo_controller/src/main.jsx
+++ demo_controller/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LogicGatesController from './logic_gates_controller/logic_gates_controller.jsx'  
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LogicGatesController />
   </StrictMode>,
 )
