baae6759d652d9ba1b7dafa1518679c4faa1e651
thegbars <gbarsotti262@gmail.com>
2025-12-03 12:46:35 -0500

    QR Code + App.jsx removed

--- demo-site/src/main.jsx
+++ demo-site/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import {BrowserRouter, Route, Routes} from "react-router";
-import App from "@/App.jsx";
 import HomePage from "@/pages/HomePage.jsx"
 import LogicGates from "@/pages/Demo1_Logic_Gates/Logic_Gates_Main.jsx";
 import SearchingSorting from "@/pages/Demo2_Searching_and_Sorting/Searching_Sorting_Main.jsx"
